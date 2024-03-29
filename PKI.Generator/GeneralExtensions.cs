d4d2eb34c9dff297596ef3ae3d91a184bf7742a2
Joseph Shook <Joseph.Shook@Surescripts.com>
2024-03-29 10:36:59 -0700

    Adding Certs and nginx for a local ssl experience.
    First commit only applied the an ssl certificate to https://keycloak .
    
    Remember to install the logica-community-sandbox-TestCA.cer in your trusted root store.  I am on Windows so mmc and my current user Trusted Root Certificate Authorities works fine.  Including the certificates generated from the pki.generator tool in the certs folder and committing to source control  Created all the certs for 10 years.

---
+++ PKI.Generator/GeneralExtensions.cs
@@ -0,0 +1,92 @@
+﻿#region (c) 2023 Joseph Shook. All rights reserved.
+// /*
+//  Authors:
+//     Joseph Shook   Joseph.Shook@Surescripts.com
+// 
+//  See LICENSE in the project root for license information.
+// */
+#endregion
+
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace pki.generator;
+
+public static class GeneralExtensions
+{
+    public static void EnsureDirectoryExists(this string source)
+    {
+        if (!Directory.Exists(source))
+        {
+            Directory.CreateDirectory(source);
+        }
+    }
+
+    public static void EnsureDirectoryExistFromFilePath(this string source)
+    {
+        var directoryPath = Path.GetDirectoryName(source);
+        if (directoryPath != null)
+        {
+            EnsureDirectoryExists(directoryPath);
+        }
+    }
+
+    public static void AddAuthorityKeyIdentifier(X509Certificate2 caCert, CertificateRequest intermediateReq)
+    {
+        //
+        // Found way to generate intermediate below
+        //
+        // https://github.com/rwatjen/AzureIoTDPSCertificates/blob/711429e1b6dee7857452233a73f15c22c2519a12/src/DPSCertificateTool/CertificateUtil.cs#L69
+        // https://blog.rassie.dk/2018/04/creating-an-x-509-certificate-chain-in-c/
+        //
+
+
+        var issuerSubjectKey = caCert.Extensions?["2.5.29.14"]!.RawData;
+        var segment = new ArraySegment<byte>(issuerSubjectKey!, 2, issuerSubjectKey!.Length - 2);
+        var authorityKeyIdentifier = new byte[segment.Count + 4];
+        // these bytes define the "KeyID" part of the AuthorityKeyIdentifier
+        authorityKeyIdentifier[0] = 0x30;
+        authorityKeyIdentifier[1] = 0x16;
+        authorityKeyIdentifier[2] = 0x80;
+        authorityKeyIdentifier[3] = 0x14;
+        segment.CopyTo(authorityKeyIdentifier, 4);
+        intermediateReq.CertificateExtensions.Add(new X509Extension("2.5.29.35", authorityKeyIdentifier, false));
+    }
+
+
+    public static X509Extension MakeCdp(string url)
+    {
+        //
+        // urls less than 119 char solution.
+        // From Bartonjs of course.
+        //
+        // https://stackoverflow.com/questions/60742814/add-crl-distribution-points-cdp-extension-to-x509certificate2-certificate
+        //
+        // From Crypt32:  .NET doesn't support CDP extension. You have to use 3rd party libraries for that. BC is ok if it works for you.
+        // Otherwise write you own. :)
+        //
+
+        byte[] encodedUrl = Encoding.ASCII.GetBytes(url);
+
+        if (encodedUrl.Length > 119)
+        {
+            throw new NotSupportedException();
+        }
+
+        byte[] payload = new byte[encodedUrl.Length + 10];
+        int offset = 0;
+        payload[offset++] = 0x30;
+        payload[offset++] = (byte)(encodedUrl.Length + 8);
+        payload[offset++] = 0x30;
+        payload[offset++] = (byte)(encodedUrl.Length + 6);
+        payload[offset++] = 0xA0;
+        payload[offset++] = (byte)(encodedUrl.Length + 4);
+        payload[offset++] = 0xA0;
+        payload[offset++] = (byte)(encodedUrl.Length + 2);
+        payload[offset++] = 0x86;
+        payload[offset++] = (byte)(encodedUrl.Length);
+        Buffer.BlockCopy(encodedUrl, 0, payload, offset, encodedUrl.Length);
+
+        return new X509Extension("2.5.29.31", payload, critical: false);
+    }
+}
---
+++ PKI.Generator/Program.cs
@@ -0,0 +1,226 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+using System.Resources;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Crypto.Operators;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.X509;
+using Org.BouncyCastle.X509.Extension;
+using pki.generator;
+using X509Extension = System.Security.Cryptography.X509Certificates.X509Extension;
+using X509Extensions = Org.BouncyCastle.Asn1.X509.X509Extensions;
+
+Console.WriteLine("Generating logicahealth PKI for local development");
+
+MakeCa();
+
+foreach (var sslProxyCert in SSLProxyCerts())
+{
+    MakeEndCert(sslProxyCert[0], sslProxyCert[1]);
+}
+
+
+void MakeCa()
+{
+    using (RSA parentRSAKey = RSA.Create(4096))
+    {
+        var parentReq = new CertificateRequest(
+            "CN=logica-community-sandbox-TestCA, OU=Root, O=FhirLabs, L=Portland, S=Oregon, C=US",
+            parentRSAKey,
+            HashAlgorithmName.SHA256,
+            RSASignaturePadding.Pkcs1);
+
+        parentReq.CertificateExtensions.Add(
+            new X509BasicConstraintsExtension(true, false, 0, true));
+
+        parentReq.CertificateExtensions.Add(
+            new X509KeyUsageExtension(
+                X509KeyUsageFlags.CrlSign | X509KeyUsageFlags.KeyCertSign,
+                true));
+
+        parentReq.CertificateExtensions.Add(
+            new X509EnhancedKeyUsageExtension(
+                new OidCollection
+                {
+                    new Oid("1.3.6.1.5.5.7.3.2"), // TLS Client auth
+                    new Oid("1.3.6.1.5.5.7.3.1"), // TLS Server auth
+                    new Oid("1.3.6.1.5.5.7.3.8") // Time Stamping
+                },
+                true));
+
+        parentReq.CertificateExtensions.Add(
+            new X509SubjectKeyIdentifierExtension(parentReq.PublicKey, false));
+
+        using (var caCert = parentReq.CreateSelfSigned(
+                   DateTimeOffset.UtcNow.AddDays(-1),
+                   DateTimeOffset.UtcNow.AddYears(10)))
+        {
+            var parentBytes = caCert.Export(X509ContentType.Pkcs12, "udap-test");
+            SureFhirLabsCertStore().EnsureDirectoryExists();
+            File.WriteAllBytes($"{SureFhirLabsCertStore()}/logica-community-sandbox-TestCA.pfx", parentBytes);
+            char[] caPem = PemEncoding.Write("CERTIFICATE", caCert.RawData);
+            File.WriteAllBytes($"{SureFhirLabsCertStore()}/logica-community-sandbox-TestCA.cer",
+                caPem.Select(c => (byte)c).ToArray());
+            // UpdateWindowsMachineStore(caCert);
+
+        }
+    }
+}
+
+
+void MakeEndCert(string dn, string san)
+{
+    using var rootCA = new X509Certificate2($"{SureFhirLabsCertStore()}/logica-community-sandbox-TestCA.pfx", "udap-test");
+
+    $"{SureFhirLabsCertStore()}/ssl".EnsureDirectoryExists();
+
+    BuildSslCertificate(
+        rootCA,
+        dn,
+        san,
+        $"{SureFhirLabsCertStore()}/ssl/{san}"
+    );
+}
+
+X509Certificate2 BuildSslCertificate(
+        X509Certificate2? caCert,
+        string distinguishedName,
+        string subjectAltNames,
+        string sslCertFilePath,
+        string? crl = default,
+        // string? buildAIAExtensionsPath = null,
+        DateTimeOffset notBefore = default,
+        DateTimeOffset notAfter = default)
+{
+
+    if (notBefore == default)
+    {
+        notBefore = DateTimeOffset.UtcNow;
+    }
+
+    if (notAfter == default)
+    {
+        notAfter = DateTimeOffset.UtcNow.AddYears(10);
+    }
+
+
+    using RSA rsaKey = RSA.Create(2048);
+
+    var sslRequest = new CertificateRequest(
+        distinguishedName,
+        rsaKey,
+        HashAlgorithmName.SHA256,
+        RSASignaturePadding.Pkcs1);
+
+    sslRequest.CertificateExtensions.Add(
+        new X509BasicConstraintsExtension(false, false, 0, true));
+
+    sslRequest.CertificateExtensions.Add(
+        new X509KeyUsageExtension(
+            X509KeyUsageFlags.DigitalSignature,
+            true));
+
+    sslRequest.CertificateExtensions.Add(
+        new X509SubjectKeyIdentifierExtension(sslRequest.PublicKey, false));
+
+    GeneralExtensions.AddAuthorityKeyIdentifier(caCert, sslRequest);
+
+    if (crl != null)
+    {
+        sslRequest.CertificateExtensions.Add(GeneralExtensions.MakeCdp(crl));
+    }
+
+    var subAltNameBuilder = new SubjectAlternativeNameBuilder();
+    subAltNameBuilder.AddDnsName(subjectAltNames);
+    var x509Extension = subAltNameBuilder.Build();
+    sslRequest.CertificateExtensions.Add(x509Extension);
+
+    sslRequest.CertificateExtensions.Add(
+        new X509EnhancedKeyUsageExtension(
+            new OidCollection {
+                    new Oid("1.3.6.1.5.5.7.3.2"), // TLS Client auth
+                    new Oid("1.3.6.1.5.5.7.3.1"), // TLS Server auth
+            },
+            true));
+
+    var clientCert = sslRequest.Create(
+        caCert,
+        notBefore,
+        notAfter,
+        new ReadOnlySpan<byte>(RandomNumberGenerator.GetBytes(16)));
+    // Do something with these certs, like export them to PFX,
+    // or add them to an X509Store, or whatever.
+    var clientCertWithKey = clientCert.CopyWithPrivateKey(rsaKey);
+
+
+    var certPackage = new X509Certificate2Collection();
+    certPackage.Add(clientCertWithKey);
+    certPackage.Add(new X509Certificate2(caCert.Export(X509ContentType.Cert)));
+
+    var clientBytes = certPackage.Export(X509ContentType.Pkcs12, "udap-test");
+    File.WriteAllBytes($"{sslCertFilePath}.pfx", clientBytes);
+    var clientPem = PemEncoding.Write("CERTIFICATE", clientCert.RawData);
+    File.WriteAllBytes($"{sslCertFilePath}.cer", clientPem.Select(c => (byte)c).ToArray());
+    File.WriteAllBytes($"{sslCertFilePath}.pem", clientPem.Select(c => (byte)c).ToArray());
+
+    var key = PemEncoding.Write("RSA PRIVATE KEY", rsaKey.ExportRSAPrivateKey());
+    File.WriteAllBytes($"{sslCertFilePath}.key", key.Select(c => (byte)c).ToArray());
+
+    return clientCert;
+}
+
+
+static string BaseDir()
+{
+    var assembly = Assembly.GetExecutingAssembly();
+    var resourcePath = String.Format(
+        $"{Regex.Replace(assembly.ManifestModule.Name, @"\.(exe|dll)$", string.Empty, RegexOptions.IgnoreCase)}" +
+        $".Resources.ProjectDirectory.txt");
+
+    var rm = new ResourceManager("Resources", assembly);
+    using var stream = assembly.GetManifestResourceStream(resourcePath);
+    using var streamReader = new StreamReader(stream!);
+
+    return streamReader.ReadToEnd().Trim();
+}
+
+
+static string SureFhirLabsCertStore()
+{
+    return $"{BaseDir()}/certstores/nginx_proxy_ssl";
+}
+
+
+static IEnumerable<List<string>> SSLProxyCerts()
+{
+    yield return new List<string>
+    {
+            "CN=keycloak",                  //DistinguishedName
+            "keycloak"                      //SubjAltName
+    };
+
+    yield return new List<string>
+    {
+            "CN=sandbox-manager-api",       //DistinguishedName
+            "sandbox-manager-api"           //SubjAltName
+    };
+
+    yield return new List<string>
+    {
+            "CN=r4",                        //DistinguishedName
+            "r4"                            //SubjAltName
+    };
+
+    yield return new List<string>
+    {
+            "CN=r5",                        //DistinguishedName
+            "r5"                            //SubjAltName
+    };
+}
+
